cbac5ce058e12f872f29fbfe85cd6586c5612c79
Leon_Irving <31895017+LeonIrving@users.noreply.github.com>
2021-02-26 16:50:54 +0000

    Enemy Spawning System
    **Updates**
    - Started implementation of the enemy spawning system
    - EnemyGenerator takes in an enemyPrefab and communicates with SpawnZones to randomly spawn enemies throughout the map on a timer

---
+++ Assets/Main/Scripts/Core/SpawnZone.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SpawnZone : MonoBehaviour
+{
+    public float radius = 1;
+    private bool isAdded = false;
+
+    private void Awake()
+    {
+        radius = GetComponent<SphereCollider>().radius;
+    }
+
+    private void LateUpdate()
+    {
+        if (!isAdded && EnemyGenerator.instance != null)
+        {
+            EnemyGenerator.instance.AddZone(this);
+            isAdded = true;
+        }
+    }
+}
